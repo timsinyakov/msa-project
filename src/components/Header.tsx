ddc147f12c3d02818221e587d462b696b123b1b1
timsuh <timsinyakov13@gmail.com>
2024-07-09 16:57:48 +1200

    fixed bugs

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import {
   useComputedColorScheme,
   useMantineColorScheme,
   useMantineTheme,
-
+  Text,
 } from '@mantine/core';
 import styles from './Header.module.css';
 import { FaSun, FaMoon } from 'react-icons/fa';
@@ -13,7 +13,7 @@ import { CgProfile } from 'react-icons/cg';
 import { LuDoorOpen } from 'react-icons/lu';
 import { MdLogin } from 'react-icons/md';
 import { Link } from 'react-router-dom';
-
+import { getAuth, signOut } from 'firebase/auth';
 
 export function Header() {
   const { setColorScheme } = useMantineColorScheme();
@@ -24,7 +24,51 @@ export function Header() {
     setColorScheme(theme === 'dark' ? 'light' : 'dark');
   };
 
+  const auth = getAuth();
+  const thisthing = () => {
+    const user = auth.currentUser;
+    if (user) {
+      console.log(user.email);
+    }
 
+    signOut(auth)
+      .then(() => {})
+      .catch((error) => {});
+  };
+
+  return (
+    <>
+      <Center style={{ padding: '20px' }}>
+        <Group justify="space-between" w="800px">
+          <Group>
+            <Button
+              variant="outline"
+              color={theme === 'dark' ? 'white' : 'black'}
+              onClick={toggleColorScheme}
+              style={{ color: theme === 'dark' ? 'white' : 'black' }}
+            >
+              {theme === 'dark' ? (
+                <>
+                  <FaSun />
+                </>
+              ) : (
+                <>
+                  <FaMoon />
+                </>
+              )}
+            </Button>
+            <Button
+              variant="outline"
+              color={theme === 'dark' ? 'white' : 'black'}
+              component={Link}
+              to="/profile"
+            >
+              <CgProfile />
+            </Button>
+          </Group>
+          <Text component={Link} to="/" size="30px">
+            RUN JOURNAL
+          </Text>{' '}
           <Group>
             <Button
               variant="outline"
@@ -37,7 +81,7 @@ export function Header() {
             <Button
               variant="outline"
               color={theme === 'dark' ? 'white' : 'black'}
-
+              onClick={() => thisthing()}
             >
               <LuDoorOpen />
             </Button>
