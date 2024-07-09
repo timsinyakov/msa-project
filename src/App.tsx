5217b59059a894c45d1438136570c8b5a85ac6d8
timsuh <timsinyakov13@gmail.com>
2024-07-09 16:47:48 +1200

    Revert "Merge pull request #5 from timsinyakov/login"
    This reverts commit ea29834cccafa5412f8f780e801b0647f0cb32f5, reversing
    changes made to 8a1be0bd5c4c1c950072a4441c49445ce2744317.

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,11 @@ import '@mantine/core/styles.css';
 import { MantineProvider } from '@mantine/core';
 import { Router } from './Router';
 import { theme } from './theme';
-import { onAuthStateChanged } from 'firebase/auth';
-import { Auth0Provider } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
-import { initializeApp } from 'firebase/app';
-import { config } from './config/config';
 
-initializeApp(config.firebaseConfig);
-
-export interface IApplicationProps {}
-
-const Application: React.FunctionComponent<IApplicationProps> = (prop) => {
+export default function App() {
   return (
     <MantineProvider theme={theme}>
       <Router />
     </MantineProvider>
   );
-};
-
-export default Application;
+}
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,11 +1,13 @@
-import { Header } from '@/components/Header';
-import { Navbar } from '@/components/Nav';
+import { Header } from "@/components/Header";
+import { Navbar } from "@/components/Nav";
 
-export function About() {
-  return (
-    <>
-      <Header />
-      <Navbar />
-    </>
-  );
+
+export function About(){
+    return (
+        <>
+        
+            <Header />
+            <Navbar />
+        </>
+    )
 }
