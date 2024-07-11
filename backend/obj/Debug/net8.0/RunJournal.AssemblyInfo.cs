e107fcfb94fb9f53822589c58ebdda66e2d84751
timsuh <timsinyakov13@gmail.com>
2024-07-11 19:13:36 +1200

    added gitignore files

--- backend/obj/Debug/net8.0/RunJournal.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RunJournal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed261e5bf8a7501b12764f36e9de670f0387b694")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2da11b12c8d41db849cacf8cf7ced26eb1ff1503")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
