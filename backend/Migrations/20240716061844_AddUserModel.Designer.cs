c44451819cb60be91e85c019cc7d5285197f66a4
timsuh <timsinyakov13@gmail.com>
2024-07-16 18:22:44 +1200

    added user table to db
    new table and added finding runs based on user id

--- backend/Migrations/20240716061844_AddUserModel.Designer.cs
+++ backend/Migrations/20240716061844_AddUserModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RunJournal.Data;
 
@@ -11,9 +12,11 @@ using RunJournal.Data;
 namespace RunJournal.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20240716061844_AddUserModel")]
+    partial class AddUserModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- backend/obj/Debug/net8.0/RunJournal.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/RunJournal.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e107fcfb94fb9f53822589c58ebdda66e2d84751")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bff2812f3fc2c929a127f390456a8392e3de757")]
 [assembly: System.Reflection.AssemblyProductAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RunJournal")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
