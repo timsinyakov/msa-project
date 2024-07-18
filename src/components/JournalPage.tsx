eea570f55d9cb7f3de5c14ea3c0e939e326fc99c
timsuh <timsinyakov13@gmail.com>
2024-07-18 14:32:16 +1200

    made journal map the runs fetched

--- src/components/JournalPage.tsx
+++ src/components/JournalPage.tsx
@@ -14,16 +14,34 @@ export function JournalPage() {
   };
 
   return (
-    <div>
-      <Button onClick={handleSubmit}></Button>
-      <h2>Run List</h2>
-      <ul>
-        {runs.map((run) => (
-          <div>{JSON.stringify(run.id)}</div>
-        ))}
-      </ul>
+    <>
+      <Center>
+        <div>
+          {runs.map((run) => (
+            <div className={styles.singleRun}>
+              <Group justify="center" grow>
+                <Text style={{ paddingLeft: '12px', paddingBottom: '100px' }}>Run {run.id}</Text>
+                <Stack>
+                  <Text>Distance: {run.distance}</Text>
+                  <Text>time: {run.time}</Text>
+                  <Text>speed: {run.distance / run.time}</Text>
+                </Stack>
 
-      {JSON.stringify(runs[8])}
-    </div>
+                <Stack>
+                  <Text>enjoyment: {run.enjoyment}</Text>
+                  <Text>difficulty: {run.difficulty}</Text>
+                  <Text>pain: {run.pain}</Text>
+                  <Text>effort: {run.effort}</Text>
+                </Stack>
+
+                <Text>Notes: {run.note}</Text>
+              </Group>
+            </div>
+          ))}
+        </div>
+      </Center>
+      <Button onClick={handleSubmit}> get runs</Button>
+      <Text>y</Text>
+    </>
   );
 }
