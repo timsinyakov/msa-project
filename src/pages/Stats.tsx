3015348e8ef28e767d535ab3078d9cf74e3c3089
timsuh <timsinyakov13@gmail.com>
2024-07-25 22:34:53 +1200

    added stats page

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,13 +1,13 @@
-import { Header } from "@/components/Header";
-import { Navbar } from "@/components/Nav";
+import { AllStats } from '@/components/AllStats';
+import { Header } from '@/components/Header';
+import { Navbar } from '@/components/Nav';
 
-
-export function Stats(){
-    return (
-        <>
-        
-            <Header />
-            <Navbar />
-        </>
-    )
+export function Stats() {
+  return (
+    <>
+      <Header />
+      <Navbar />
+      <AllStats />
+    </>
+  );
 }
