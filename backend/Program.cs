7b86afb1334855acf967868a51d75fa096700d61
timsuh <timsinyakov13@gmail.com>
2024-08-01 20:14:43 +1200

    added dockerfile for frontend and allowed port

--- backend/Program.cs
+++ backend/Program.cs
@@ -22,7 +22,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowReactApp",
         policy =>
         {
-            policy.WithOrigins("http://localhost:5173", "http://localhost:4173")
+            policy.WithOrigins("http://localhost:5173", "http://localhost:4173", "http://localhost:3000")
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
